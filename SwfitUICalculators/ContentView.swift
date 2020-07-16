f6bffe06be6c04e845fc77acf58992afaf397d6c
woshahua <wohangye@gmail.com>
2020-07-17 01:00:54 +0900

    first text view

---
+++ SwfitUICalculators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwfitUICalculators
+//
+//  Created by HANG GAO on 2020/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("+")
+            .font(.title)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.orange)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
